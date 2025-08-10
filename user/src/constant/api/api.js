81076c99ac119b92b1ea362e590793431dcabb61
Lập Nguyễn <lapthd2k3@gmail.com>
2025-08-10 20:16:03 +0700

    tracking

--- user/src/constant/api/api.js
+++ user/src/constant/api/api.js
@@ -1,6 +1,7 @@
 export const API_DOMAIN = "https://sep490-dabs-gsdjgbfbdgd8gkbb.eastasia-01.azurewebsites.net";
 export const PATH = "/api/v1";
 
+// tracking
 export const API_PROVINCES = "https://vietnamlabs.com/api/vietnamprovince";
 export const API_DISTRICTS = (provinceCode) => `https://provinces.open-api.vn/api/p/${provinceCode}?depth=2`;
 export const API_WARDS = (districtCode) => `https://provinces.open-api.vn/api/d/${districtCode}?depth=2`;
